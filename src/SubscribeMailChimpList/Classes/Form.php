2b87cdadf6e35a7d2e4f1ced0736fbf3750c02c9
petrozvodsky <petrozavodsky@gmail.com>
2018-05-17 17:00:44 +0300

    Form html method

--- src/SubscribeMailChimpList/Classes/Form.php
+++ src/SubscribeMailChimpList/Classes/Form.php
@@ -6,4 +6,16 @@ namespace SubscribeMailChimpList\Classes;
 
 class Form {
 
+	public function __construct() {
+
+		add_filter( 'SubscribeMailChimpList__shortcode-content', [ $this, 'form_html' ] );
+	}
+
+
+	public function form_html( $attrs ) {
+		?>
+
+		<?php
+	}
+
 }
