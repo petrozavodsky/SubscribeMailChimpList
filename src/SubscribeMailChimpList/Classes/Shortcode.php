2216852ac6dca2f3100b3e82d97242ccad8bc898
petrozvodsky <petrozavodsky@gmail.com>
2018-05-17 16:56:58 +0300

    Shortcode class

--- src/SubscribeMailChimpList/Classes/Shortcode.php
+++ src/SubscribeMailChimpList/Classes/Shortcode.php
@@ -11,21 +11,10 @@ class Shortcode extends ActivateShortcode {
 	protected $js = false;
 	protected $css = true;
 
-	function init( $tag, $attrs ) {
-		add_action( "template_redirect", function () use ( $tag ) {
-			global $wp_query;
-			if ( is_singular() && has_shortcode( $wp_query->post->post_content, $tag ) ) {
-				$this->addCss($tag);
-			}
-		} );
-	}
 
 	function base( $attrs, $content, $tag ) {
-		$json = json_encode( $attrs );
-		$res  = "";
-		$res  .= "<pre>{$json}</pre>";
 
-		return $res;
+		return apply_filters( 'SubscribeMailChimpList__shortcode-content', $attrs );
 	}
 
 }
