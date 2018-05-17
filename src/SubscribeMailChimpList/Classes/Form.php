46bda58006a0fc59317e912543f9c5620c2f8075
petrozvodsky <petrozavodsky@gmail.com>
2018-05-17 17:05:52 +0300

    args

--- src/SubscribeMailChimpList/Classes/Form.php
+++ src/SubscribeMailChimpList/Classes/Form.php
@@ -8,14 +8,48 @@ class Form {
 
 	public function __construct() {
 
-		add_filter( 'SubscribeMailChimpList__shortcode-content', [ $this, 'form_html' ] );
+		add_filter( 'SubscribeMailChimpList__shortcode-content', [ $this, 'form_html' ] ,10 ,1 );
 	}
 
 
 	public function form_html( $attrs ) {
+	    d($attrs);
+		ob_start();
 		?>
+        <div class="SubscribeMailChimpList__form-wrapper">
+            <div class="SubscribeMailChimpList__form-wrap">
 
+                <div class="SubscribeMailChimpList__form-title">
+					<?php echo $attrs['title']; ?>
+                </div>
+
+                <form action="" method="post">
+
+
+                    <div class="SubscribeMailChimpList__form-fields-wrapper">
+
+                        <input type="email" class="SubscribeMailChimpList__form-input">
+
+                        <button type="submit" class="SubscribeMailChimpList__form-button">
+							<?php _e( 'Subscribe', 'SubscribeMailChimpList' ); ?>
+                        </button>
+
+                    </div>
+
+
+                    <div class="SubscribeMailChimpList__form-alerts-wrap">
+
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
 		<?php
+		$res = ob_get_contents();
+		ob_clean();
+
+		return $res;
 	}
 
 }
