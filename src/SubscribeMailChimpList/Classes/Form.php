cb18c15be0f1cad72cc9c5dd3b6488df0388cc6f
petrozvodsky <petrozavodsky@gmail.com>
2018-05-21 11:02:08 +0300

    Add hook

--- src/SubscribeMailChimpList/Classes/Form.php
+++ src/SubscribeMailChimpList/Classes/Form.php
@@ -8,7 +8,10 @@ class Form {
 
 	public function __construct() {
 
-		add_filter( 'SubscribeMailChimpList__shortcode-content', [ $this, 'form_html' ] ,10 ,1 );
+		add_filter( 'SubscribeMailChimpList__shortcode-content', [ $this, 'form_html' ], 10, 1 );
+
+		add_action( 'SubscribeMailChimpList__form', [ $this, 'form_html' ], 10, 1 );
+
 	}
 
 
@@ -27,7 +30,8 @@ class Form {
 
                     <div class="SubscribeMailChimpList__form-fields-wrapper">
 
-                        <input type="email" placeholder="<?php _e('Enter your E-mail...','SubscribeMailChimpList');?>" class="SubscribeMailChimpList__form-input">
+                        <input type="email" placeholder="<?php _e( 'Enter your E-mail...', 'SubscribeMailChimpList' ); ?>"
+                               class="SubscribeMailChimpList__form-input">
 
                         <button type="submit" class="SubscribeMailChimpList__form-button">
 							<?php _e( 'Subscribe', 'SubscribeMailChimpList' ); ?>
