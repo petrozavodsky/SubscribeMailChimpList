e34e48b897242429e35568642551b9e598e3ce87
petrozvodsky <petrozavodsky@gmail.com>
2018-05-22 12:05:38 +0300

    Action selector

--- public/js/SubscribeMailChimpList-MailChimpListAddMail.js
+++ public/js/SubscribeMailChimpList-MailChimpListAddMail.js
@@ -1,12 +1,51 @@
 var SubscribeMailChimpListMailChimpListAddMail =
     {
-        slider: false,
+
+        action_url: false,
+
         selector: '.grid__girl-posts-items',
+
         run: function () {
+
             var this_class = this;
-            this_class.init("")
+
+            this_class.action_url = window.SubscribeMailChimpList_MailChimpListAddMail__vars.ajax_url_action;
+
+            this_class.init("[action$='" + this_class.action_url + "']");
+
+
         },
         init: function (selector) {
+
+
+            $(selector).submit(function (e) {
+
+                e.preventDefault();
+
+                var method = $(this).attr('method');
+                var action = $(this).attr('action');
+                var form = $(this);
+                var data = $(this).serialize();
+                var button = $(this).find("[type='submit']");
+
+                button.removeAttr('disabled');
+
+
+                $.ajax({
+                    type: method,
+                    url: action,
+                    data: data,
+                    beforeSend: function () {
+                        button.attr('disabled', 'disabled');
+                    },
+                    success: function (result) {
+                        button.removeAttr('disabled');
+
+                        console.log(result);
+                    }
+                });
+
+            });
 
         },
 
